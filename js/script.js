d727d7423f9c2b2bbb0dda0389641df0dc93ee2a
Thiago-Caffaro <96150887+Thiago-Caffaro@users.noreply.github.com>
2022-12-20 20:27:06 -0300

    .

--- js/script.js
+++ js/script.js
@@ -27,6 +27,9 @@ function abrirPasta(type) {
                     <tr onClick="abrirDiretorio('Lixeira')">
                         <td>Lixeira</td>
                     </tr>
+                    <tr onClick="abrirDiretorio('Users')">
+                        <td>Usuários</td>
+                    </tr>
                 </table>
             </div>
             <div id="main-arquivo">
@@ -54,18 +57,37 @@ function fecharJanela(){
 }
 function abrirVideo(video){
     const videoHtml = `
-        <div id="janela">
-            <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div>
-            <video width="100%" height="570px" autoplay><source src=${video} type="video/mp4"></video>
+    <div class="box"  onclick="flip()">
+            <div id="box__content">
+            <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div><table>
+                <div class="box__front">
+                    <video width="100%" autoplay><source src=${video} type="video/mp4"></video>
+                </div>
+                <div class="box__back" style="background-color: aliceblue;"></div>   
+            </div>
         </div>
+    </div>
     `
     document.getElementById('placeHolderJanela').innerHTML = videoHtml
 }
-function abrirImagem(imagem){
+function abrirImagem(imagem, papel){
+    let src = '';
+    if (papel == 'velho'){
+        src = "default.png"
+    }else if (papel == 'novo'){
+        src = "default2.png"
+    }
     const imagemHtml = `
-        <div id="janela">
-            <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div>
-            <img src=${imagem} style="width: 100%; height: 620px;">
+        <div class="box"  onclick="flip()">
+            <div id="box__content">
+            <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div><table>
+                <div class="box__front">
+                    <img src="${imagem}">
+                </div>
+                <div class="box__back">
+                    <img src="images/${src}">
+                </div>
+            </div>
         </div>
     `
     document.getElementById('placeHolderJanela').innerHTML = imagemHtml
@@ -74,8 +96,8 @@ function abrirAudio(audio){
     const audioHtml = `
         <div id="janela">
             <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div>
-            <img src=${imagem} style="width: 100%; height: 620px;">
-            <audio src="${audio}" autoplay></audio>
+            <img src="images/icons/icons8-audio-100a.png" style="margin-left: 500px; margin-top: 200px;"><br>
+            <audio src="${audio}" controls style="margin-left: 400px;"></audio>
         </div>
     `
     document.getElementById('placeHolderJanela').innerHTML = audioHtml
@@ -84,8 +106,23 @@ function abrirDiretorio(type){
     if (type === 'areaTrabalho'){
         const dirHtml = `
         <table style="width:100%">
-            <tr>
-                <td>Pasta vazia1</td>
+            <tr onclick="abrirPasta('arquivo'), abrirDiretorio('Lixeira')">
+                <td>Lixeira</td>
+            </tr>
+            <tr onclick="abrirImagem('images/cato.png', 'novo')">
+                <td>Meu amor1.png</td>
+            </tr>
+            <tr onclick="abrirVideo('images/pexe.mp4')">
+                <td>Meu amor2.png</td>
+            </tr>
+            <tr onclick="abrirVideo('images/miau.mp4')">
+                <td>Meu amor3.png</td>
+            </tr>
+            <tr onclick="abrirAudio('ult.mp3')">
+                <td>Corra.mp3</td>
+            </tr>
+            <tr onclick="abrirPasta('arquivo')">
+                <td>Felipe Oliveira</td>
             </tr>
         </table>
         `
@@ -95,7 +132,7 @@ function abrirDiretorio(type){
         const dirHtml = `
             <table style="width:100%">
                 <tr>
-                    <td>Pasta vazia2</td>
+                    <td>Pasta vazia</td>
                 </tr>
             </table>
         `
@@ -105,7 +142,10 @@ function abrirDiretorio(type){
         const dirHtml = `
             <table style="width:100%">
                 <tr>
-                    <td>Pasta vazia3</td>
+                    <td onclick="abrirVideo('images/pexe.mp4')">Meu amor2.mp4</td>
+                </tr>
+                <tr>
+                    <td onclick="abrirVideo('images/cato.mp4')">Meu amor3.mp4</td>
                 </tr>
             </table>    
         `
@@ -115,7 +155,7 @@ function abrirDiretorio(type){
         const dirHtml = `
             <table style="width:100%">
                 <tr>
-                    <td>Pasta vazia4</td>
+                    <td onclick="abrirImagem('images/cato.png')">Meu amor1.png</td>
                 </tr>
             </table> 
         `
@@ -131,4 +171,54 @@ function abrirDiretorio(type){
         `
         document.getElementById('main-arquivo').innerHTML = dirHtml
     }
+    if (type === "Users"){
+        const dirHtml = `
+            <table style="width:100%">
+                <tr>
+                    <td onclick="user('felipe')">Felipe Oliveira</td>
+                </tr>
+                <tr>
+                    <td>Olavo Oliveira</td>
+                </tr>
+                <tr>
+                    <td>Carlonso Oliveira</td>
+                </tr>
+                <tr>
+                    <td>Heremita Oliveira</td>
+                </tr>
+                <tr>
+                    <td>Bolongo Oliveira</td>
+                </tr>
+            </table> 
+        `
+        document.getElementById('main-arquivo').innerHTML = dirHtml
+    }
+}
+
+function user(user){
+    if (user === 'felipe'){
+        const usrHtml = `
+        <div id="janela">
+        <div id="barra-janela-cima"><button onClick="fecharJanela()"><img src="images/icons/x_icon_172101.png"></button></div>
+            <table style="width:100%">
+                <tr onclick="abrirImagem('images/gay.png', 'velho')">
+                    <td>Declaração</td>
+                </tr>
+                <tr onclick="abrirImagem('images/guriaremaster.png', 'novo')">
+                    <td>Ela</td>
+                </tr>
+                <tr onclick="abrirImagem('images/morte.png', 'velho')">
+                    <td>...</td>
+                </tr>
+            </table>
+        </div>
+        `
+        document.getElementById('janela').innerHTML = usrHtml
+    }
+
+}
+function flip(){
+    var element = document.getElementById("box__content");
+    element.classList.toggle("flip");
+
 }
